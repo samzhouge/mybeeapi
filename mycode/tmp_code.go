8217a33d5578fcf3a3787ba4b5362053f94714ee
周戈-mac <xiaotiantang_1110@126.com>
2023-03-17 17:13:29 +0800

    update go

--- mycode/tmp_code.go
+++ mycode/tmp_code.go
@@ -12,8 +12,32 @@ func convertToBin(v int) (result string) {
 	return
 }
 
+func sumArgs(numbers ...int) int {
+	sum := 0
+	for _, v := range numbers {
+		sum += v
+	}
+	return sum
+}
+
+func printSlice(s []int) {
+	fmt.Printf("%v, len=%d, cap=%d\n", s, len(s), cap(s))
+}
+
 func main() {
 	fmt.Println(convertToBin(5))
 	fmt.Println(convertToBin(13))
 	fmt.Println(fmt.Errorf("%s", "a"))
+
+	fmt.Println(sumArgs(1, 2, 3))
+
+	s1 := []int{2, 4, 6, 8}
+	s2 := make([]int, 10, 32)
+
+	copy(s2, s1)
+	printSlice(s2)
+
+	s2 = append(s2[:3], s2[4:]...)
+	printSlice(s2)
+
 }
